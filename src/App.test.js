04348b2e3a00cb490ccc361c82fc51655d04b35c
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-09-27 10:52:45 -0400

    Cuarta prueba

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import calculateTotal from "./App.js";
 
 describe("Calcular Total", () => {
   it("deberia sacar el Precio Total", () => {
-    expect(calculateTotal(1, 1, "NV")).toEqual(2.065);
+    expect(calculateTotal(1, 1, "NV")).toEqual(1.08);
   });
   it("deberia sacar el Precio Total", () => {
     expect(calculateTotal(2, 2, "NV")).toEqual(4.32);
