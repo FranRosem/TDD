f9d5919f687da455cf5d10c87d14276bedc9fa82
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-09-27 10:46:20 -0400

    Segunda prueba

--- src/App.test.js
+++ src/App.test.js
@@ -4,4 +4,7 @@ describe("Calcular Total", () => {
   it("deberia sacar el Precio Total", () => {
     expect(calculateTotal(1, 1, "NV")).toEqual(2.065);
   });
+  it("deberia sacar el Precio Total", () => {
+    expect(calculateTotal(2, 2, "NV")).toEqual(4.32);
+  });
 });
